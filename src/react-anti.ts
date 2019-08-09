b837e1f28ddac05cd674798b1a0a766aec8f6bdc
Vincent <const.lomo@gmail.com>
2019-08-09 18:33:10 +0800

    fix

--- src/react-anti.ts
+++ src/react-anti.ts
@@ -1,5 +1,4 @@
 import React, { ReactElement, RefObject } from 'react';
-import ReactDOM from 'react-dom';
 
 export interface AntiComponentProps<T = any> {
   resolve?: (value?: T | PromiseLike<T>) => void;
@@ -17,48 +16,8 @@ class Container extends React.Component<Props, State> {
     children: []
   };
 
-  show = (element: ReactElement) => {
-    let promiseResolve;
-    let promiseReject;
-    let cloned: ReactElement;
-    const promise: AntiPromise = new Promise((resolve, reject) => {
-      promiseResolve = resolve;
-      promiseReject = reject;
-      cloned = React.cloneElement(element, {
-        key: uniqueKey++,
-        resolve,
-        reject
-      });
-      this.setState(({ children }) => {
-        return {
-          children: [...children, cloned]
-        };
-      });
-    }).finally(() => {
-      this.setState(({ children }) => {
-        return {
-          children: children.filter(val => val !== cloned)
-        };
-      });
-    });
-    promise.resolve = promiseResolve;
-    promise.reject = promiseReject;
-    return promise;
-  };
-
-  el = document.createElement('div');
-
-  componentDidMount() {
-    this.el.setAttribute('react-anti', '');
-    (this.props.container || document.body).appendChild(this.el);
-  }
-
-  componentWillUnmount() {
-    (this.props.container || document.body).removeChild(this.el);
-  }
-
   render() {
-    return ReactDOM.createPortal(this.state.children, this.el);
+    return this.state.children;
   }
 }
 
@@ -69,9 +28,51 @@ let uniqueKey = 0;
 const Provider: React.SFC<Props> = ({ container }) =>
   React.createElement(Container, { container, ref });
 
-const anti: { (element: ReactElement): AntiPromise; Provider?: React.SFC<Props> } = (
-  element: ReactElement
-) => ref.current.show(element);
+const anti: {
+  (element: ReactElement): AntiPromise;
+  Provider?: React.SFC<Props>;
+} = (element: ReactElement) => {
+  let promiseResolve;
+  let promiseReject;
+  let cloned: ReactElement;
+
+  const onFinally = () => {
+    ref.current.setState(({ children }) => {
+      return {
+        children: children.filter(val => val !== cloned)
+      };
+    });
+  };
+  const promise: AntiPromise = new Promise((resolve, reject) => {
+    promiseResolve = resolve;
+    promiseReject = reject;
+    cloned = React.cloneElement(element, {
+      key: uniqueKey++,
+      resolve,
+      reject
+    });
+    ref.current.setState(({ children }) => {
+      return {
+        children: [...children, cloned]
+      };
+    });
+  }).then(
+    value => {
+      onFinally();
+      return value;
+    },
+    reason => {
+      onFinally();
+      throw reason;
+    }
+  );
+
+  promise.resolve = promiseResolve;
+
+  promise.reject = promiseReject;
+
+  return promise;
+};
 
 anti.Provider = Provider;
 
